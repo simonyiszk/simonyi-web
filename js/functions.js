8519244a8240e828e5f33687fae408a67130860a
Balazs <balazs.frey@gmail.com>
2019-06-20 13:21:42 +0200

    Load posts from a directory

--- js/functions.js
+++ js/functions.js
@@ -10,17 +10,36 @@ const init = async () => {
     return await response.text();
   }
 
+  function getFiles(data) {
+    console.log(data);
+    var linePattern = /<a href="[0-9]{8}\.md">[0-9]{8}\.md<\/a>/g;
+    var found = data.match(linePattern);
+    if (!found) return [];
+
+    var numberPattern = /(\d{4})(\d{2})(\d{2})/g;
+    return found
+      .map(file => file.split('"')[1])
+      .map(file => {
+        return {
+          name: file,
+          date: new Date(file.split(".")[0].replace(numberPattern, "$1-$2-$3"))
+        };
+      })
+      .sort((a, b) => b.date - a.date);
+  }
+
+  ///////////////
+  ///Események///
+  ///////////////
   var myfile = await getObject("../data/events.yml");
   var eventobj = jsyaml.load(myfile);
-  console.log(eventobj);
 
   var honapcontainer = document.getElementById("honapok");
-
   if (!honapcontainer) return;
 
-  var result = "";
+  var honapResult = "";
   eventobj.forEach(month => {
-    result += `<div class="honap">
+    honapResult += `<div class="honap">
             <div class="honap-esemenyek">
                 ${month.events
                   .map(event => {
@@ -36,7 +55,28 @@ const init = async () => {
             </div>
         </div>`;
   });
-  honapcontainer.innerHTML = result;
+  honapcontainer.innerHTML = honapResult;
+
+  ///////////
+  ///Hírek///
+  ///////////
+  var filesRaw = await getObject("../data/posts");
+  var filesList = getFiles(filesRaw);
+  filesList = filesList.map(async (file) => getObject("../data/posts/"+file.name));
+  filesList = await Promise.all(filesList);
+
+  var hircontainer = document.getElementById("hirek");
+  if (!hircontainer) return;
+/*
+  var hirResult = "";
+  filesList.forEach(hir => {
+    hirResult += `<div class="hir">
+    <div class="hircim">${}</div>
+    <div class="hirtorzs">
+
+    </div>
+  </div>`;
+  });*/
 };
 
 init();
