fddcf7c6a26fceab16b4ed1263c7c872826d036e
Freyb <balazs.frey@gmail.com>
2019-09-09 16:30:33 +0200

    Implement Services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./services.css";
 
 export default function Services() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="servicesSectionContainer">
+      <h1 className="servicesSectionTitle">Szolgáltatások</h1>
+      <div className="servicesWrapper">
+        <a
+          href="https://termek.sch.bme.hu/teremfoglalo.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div
+            class="servicesBox"
+            style={{ backgroundImage: "url(/resources/room.jpg)" }}
+          >
+            <div class="serviceBoxTitle">Teremigénylés</div>
+          </div>
+        </a>
+        <a
+          href="https://termek.sch.bme.hu/eszkozfoglalo.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div
+            class="servicesBox"
+            style={{ backgroundImage: "url(/resources/proj.jpg)" }}
+          >
+            <div class="serviceBoxTitle">Eszközigénylés</div>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
