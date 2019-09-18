bb78f488e962a9b15c5fefed62859b0c43869a01
Freyb <balazs.frey@gmail.com>
2019-09-18 13:58:13 +0200

    Style hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,5 +2,14 @@ import React from "react";
 import "./hero.css";
 
 export default function Hero() {
-  return <div className="heroBg"></div>;
+  return (
+    <div className="hero__background">
+      <div className="hero__overlay">
+        <div className="hero__imageContainer">
+          <img src="./resources/simonyi_white.svg" alt="Simonyi" />
+        </div>
+        <div className="hero__mail"><a href="mailto:info@simonyi.bme.hu">info@simonyi.bme.hu</a></div>
+      </div>
+    </div>
+  );
 }
