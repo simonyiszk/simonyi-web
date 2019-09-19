6bd0113265ec56a741827e0d9ee45e6c00c670f9
Freyb <balazs.frey@gmail.com>
2019-09-19 15:37:14 +0200

    Make it responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,20 +5,38 @@ export default function Footer() {
   return (
     <div className="footerSectionContainer">
       <div className="footer__container">
-        <div className="footerLogo__container">
-          <a href="https://svie.hu/" target="_blank" rel="noopener noreferrer">
-            <img src={`./resources/footer/sch_small.svg`} alt="Schönherz" />
-          </a>
+        <div className="footerLogo__container schlogo">
+          <div>
+            <a
+              href="https://svie.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={`./resources/footer/sch_small.svg`} alt="Schönherz" />
+            </a>
+          </div>
         </div>
         <div className="footerLogo__container">
-          <a href="https://simonyi.bme.hu/" target="_blank" rel="noopener noreferrer">
-            <img src={`./resources/footer/simonyi.svg`} alt="Simonyi" />
-          </a>
+          <div>
+            <a
+              href="https://simonyi.bme.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={`./resources/footer/simonyi.svg`} alt="Schönherz" />
+            </a>
+          </div>
         </div>
         <div className="footerLogo__container">
-          <a href="http://www.vik.bme.hu/" target="_blank" rel="noopener noreferrer">
-            <img src={`./resources/footer/vik.svg`} alt="BME-VIK" />
-          </a>
+          <div>
+            <a
+              href="http://www.vik.bme.hu/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={`./resources/footer/vik.svg`} alt="BME-VIK" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
