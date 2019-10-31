7eb279cc2a3951a01e365b9dca7d422c13cdcdc1
Freyb <balazs.frey@gmail.com>
2019-10-31 14:37:42 +0100

    Refactor Hero with Chakra

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import "./hero.css";
+import { Box, Flex, Image } from "@chakra-ui/core";
 
 export default function Hero() {
   return (
-    <div className="hero__background">
-      <div className="hero__overlay">
-        <div className="hero__imageContainer">
-          <img src="./resources/simonyi_white.svg" alt="Simonyi" />
-        </div>
-        <div className="hero__mail"><a href="mailto:info@simonyi.bme.hu">info@simonyi.bme.hu</a></div>
-      </div>
-    </div>
+    <Flex
+      height="100vh"
+      background="url('/resources/bg.jpg') no-repeat center center fixed"
+      bgSize="cover"
+      overflow="hidden"
+      justify="center"
+      alignItems="center"
+    >
+      <Box w="60%">
+        <Box>
+          <Image src="./resources/simonyi_white.svg" alt="Simonyi" />
+        </Box>
+        <Box
+          textAlign="center"
+          color="white"
+          textDecor="none"
+          fontSize={["1.5rem", "2rem", "3rem"]}
+        >
+          <a href="mailto:info@simonyi.bme.hu">info@simonyi.bme.hu</a>
+        </Box>
+      </Box>
+    </Flex>
   );
 }
