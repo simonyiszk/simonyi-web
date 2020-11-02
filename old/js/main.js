605e8696ae7cf78fbf048b0ff3c982dc9c64d9d3
Freyb <balazs.frey@gmail.com>
2020-11-02 15:41:06 +0100

    Remove old version from main branch

--- old/js/main.js
+++
@@ -1,9 +0,0 @@
-const hamburger = document.getElementsByClassName("hamburger")[0]
-const headerlistwrapper = document.getElementsByClassName("headerlistwrapper")[0]
-hamburger.onclick = function() {
-  if (headerlistwrapper.classList.contains('open')) {
-    headerlistwrapper.classList.remove('open');
-  } else {
-    headerlistwrapper.classList.add('open');
-  }
-};
